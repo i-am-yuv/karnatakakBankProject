67baa710a46b3dff4543a6ea2d7e4117ca97f521
Gyana Ranjan Rout <gyanaranjan.r@splenta.com>
2024-03-12 17:17:19 +0530

    Fi

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
+import { ToastModule } from 'primeng/toast';
+import { DropdownModule } from 'primeng/dropdown';
+import { MessageModule } from 'primeng/message';
+import { TableModule } from 'primeng/table';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuModule } from 'primeng/menu';
+import { DialogModule } from 'primeng/dialog';
+import { SidebarModule } from 'primeng/sidebar';
+import { BlockUIModule } from 'primeng/blockui';
+import { MultiSelectModule } from 'primeng/multiselect';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MenubarModule
+  ],
+  exports: [
+    FormsModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    ButtonModule,
+    CheckboxModule,
+    ToastModule,
+    DropdownModule,
+    MessageModule,
+    ButtonModule,
+    MenubarModule,
+    MenuModule,
+    TableModule,
+    DialogModule,
+    SidebarModule,
+    BlockUIModule,
+    MultiSelectModule
+  ]
+})
+export class SharedModule { }
