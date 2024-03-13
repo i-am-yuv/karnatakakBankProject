ffa7ced695d408140a6f4f53f57b5612eeef0d58
yuvraj <yuvraj.b@splenta.com>
2024-03-13 23:58:14 +0530

    branch manager UI addedd

--- src/app/landing/landing-routing.module.ts
+++ src/app/landing/landing-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { BranchManagerComponent } from './branch-manager/branch-manager.component';
 
 const routes: Routes = [
   {
     path: '',
     component: DashboardComponent
+  },
+  {
+    path: 'branchManager',
+    component: BranchManagerComponent
   }
 ];
 
